da5673d3c4dc16781266e1fecf86c5581e5b1ba5
Raul Huamani <raul.huamani.h@gmail.com>
2020-08-18 22:46:45 -0500

    RestServer - usuario - Mongo Atlas

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,22 @@
 // ==============================
 // Puerto:
 // ==============================
-
 process.env.PORT = process.env.PORT || 3000;
+
+// ==============================
+// Base de datos:
+// ==============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ==============================
+// Base de datos:
+// ==============================
+let urlDB;
+
+// if (process.env.NODE_ENV == 'dev') {
+//     urlDB = 'mongodb://localhost:27017/cafe'
+// } else {
+urlDB = 'mongodb+srv://rhuamani:60095Yuri@cluster0.goijk.gcp.mongodb.net/cafe?retryWrites=true&w=majority';
+// }
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 require('./config/config');
+
 const express = require('express');
+const mongoose = require('mongoose');
+
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -10,35 +14,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', (req, res) => {
-    res.json('get Usuario');
-});
-
-app.post('/usuario', (req, res) => {
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        })
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-});
-
-app.put('/usuario/:id', (req, res) => {
-    let id = req.params.id;
-    res.json({
-        id
+app.use(require('./routes/usuario'));
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
+    (err, res) => {
+        // .then(() => console.log('Conectado a MongoDB...'))
+        // .catch((error) => console.log('No se pudo conectar con MongoDB...'));
+        if (err) throw err;
+        console.log('Base de datos Online conectada');
     });
-});
-
-app.delete('/usuario', (req, res) => {
-    res.json('delete Usuario');
-});
+//mongoose.set('useCreateIndex', true);
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto', 3000);
