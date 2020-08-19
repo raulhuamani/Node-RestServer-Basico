02602bc359a51397f9b324450f720ea80dd42361
Raul Huamani <raul.huamani.h@gmail.com>
2020-08-18 23:15:22 -0500

    Se corrige codigos de seleccion base de datos

--- server/config/config.js
+++ server/config/config.js
@@ -13,10 +13,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ==============================
 let urlDB;
 
-// if (process.env.NODE_ENV == 'dev') {
-//     urlDB = 'mongodb://localhost:27017/cafe'
-// } else {
-urlDB = 'mongodb+srv://rhuamani:60095Yuri@cluster0.goijk.gcp.mongodb.net/cafe?retryWrites=true&w=majority';
-// }
+if (process.env.NODE_ENV == 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb+srv://rhuamani:60095Yuri@cluster0.goijk.gcp.mongodb.net/cafe?retryWrites=true&w=majority';
+}
 
 process.env.URLDB = urlDB;
