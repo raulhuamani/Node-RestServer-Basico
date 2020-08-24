284f33dca0f8b3480592556328c6d34359a3f697
Raul Huamani <raul.huamani.h@gmail.com>
2020-08-24 00:30:01 -0500

    Productos y cetgorias

--- server/config/config.js
+++ server/config/config.js
@@ -3,15 +3,22 @@
 // ==============================
 process.env.PORT = process.env.PORT || 3000;
 
+
 // ==============================
 // Entorno:
 // ==============================
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
-// ==============================
-// Vencimiento del Token:
-// ==============================
-process.env.CADUCIDAD_TOKEN = process.env.CADUCIDAD_TOKEN || 60 * 60 * 24 * 30;
+
+// ============================
+//  Vencimiento del Token
+// ============================
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 días
+process.env.CADUCIDAD_TOKEN = process.env.CADUCIDAD_TOKEN || '48h';
+
 
 // ==============================
 // SEED de autenticación:
